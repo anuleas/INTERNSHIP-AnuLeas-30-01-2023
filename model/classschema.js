1d62cc4cc62338fdeb4128579bae121bb20e0812
Anu Leas <anuannap89@gmail.com>
2023-01-30 13:37:33 +0300

    formatclassschema

--- model/classschema.js
+++ model/classschema.js
@@ -1,15 +1,8 @@
-const mongoose = require('mongoose');
-const classSchema =new mongoose.Schema({
-    pkey: { type: String, unique: true },
-   standard: { type: String,
-    required: true
-},
-    division: { type: String,
-        required: true,
-        
-    }
-    
-   
-})
-    
-module.exports= mongoose.model('Class',classSchema);
+const mongoose = require("mongoose");
+const classSchema = new mongoose.Schema({
+  pkey: { type: String, unique: true },
+  standard: { type: String, required: true },
+  division: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Class", classSchema);
