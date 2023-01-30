99a809da86f02ef3312f9a3c0050f71fa66effb2
Anu Leas <anuannap89@gmail.com>
2023-01-30 12:34:09 +0300

    change name og postman url to postmancollectionlink

--- model/studentschema.js
+++ model/studentschema.js
@@ -1,24 +1,13 @@
-const mongoose = require('mongoose');
-var Schema=mongoose.Schema
-const studentSchema =new mongoose.Schema({
+const mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+const studentSchema = new mongoose.Schema({
   pkey: { type: String, unique: true },
-    name:  
-     { type: String},
-    rollno:  { type:Number,required: true},
-    
-    mobileno: { type:Number,
-      required: true},
-    /*classId:
-    {
-        type: Schema.Types.ObjectId,
-        ref: "Class"
-      }*/
-      classId:{ type: String,required: true},
-      
-      
+  name: { type: String },
+  rollno: { type: Number, required: true },
 
-  
-})
-    
-module.exports= mongoose.model('Student',studentSchema);
- 
+  mobileno: { type: Number, required: true },
+
+  classId: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Student", studentSchema);
